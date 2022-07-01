f7944326a85e9228a25e03c1f9d5a7fbada81556
ei-bro <eibrahim1748@gmail.com>
2022-07-02 00:15:01 +0300

    fetch local jeson dene

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ import fetchProducts from "./src/fetchProducts.js";
 import { setupStore, store } from "./src/store.js";
 import display from "./src/displayProducts.js";
 import { getElement } from "./src/utils.js";
+
+const init = async () => {
+    const products = await fetchProducts();
+    // console.log(products);
+    if (products) {
+        // add products to the store
+        setupStore(products);
+        console.log(store);
+        const featured = store.filter((product) => product.featured === true);
+        display(featured, getElement(".featured-center"));
+    }
+};
+window.addEventListener("DOMContentLoaded", init);
--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,13 @@
-import { allProductsUrl } from './utils.js';
-
-const fetchProducts = async () => {};
+// import { allProducts } from "../data/allProducts.js";
+const fetchProducts = async () => {
+    const response = await fetch("../data/allProducts.json").catch((err) =>
+        console.log(err)
+    );
+    if (response) {
+        // console.log(response);
+        return response.json();
+    }
+    return response;
+};
 
 export default fetchProducts;
