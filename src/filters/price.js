8f0f1d427717a31d3eb7c478743cbcd8162059e0
ei-bro <eibrahim1748@gmail.com>
2022-09-20 13:50:43 +0300

    card setupdone

--- src/filters/price.js
+++ src/filters/price.js
@@ -2,33 +2,33 @@ import { getElement } from "../utils.js";
 import display from "../displayProducts.js";
 
 const setupPrice = (store) => {
-    const priceInput = getElement(".price-filter");
-    const priceValue = getElement(".price-value");
+	const priceInput = getElement(".price-filter");
+	const priceValue = getElement(".price-value");
 
-    // setup filter
-    let maxPrice = store.map((product) => product.price);
-    maxPrice = Math.max(...maxPrice);
-    console.log(maxPrice);
-    maxPrice = Math.ceil(maxPrice / 10);
-    priceInput.value = maxPrice;
-    priceInput.max = maxPrice;
-    priceInput.min = 0;
-    priceValue.innerHTML = `Value : <b>ETB</b> - ${maxPrice}`;
-    console.log(maxPrice);
+	// setup filter
+	let maxPrice = store.map((product) => product.price);
+	maxPrice = Math.max(...maxPrice);
+	// console.log(maxPrice);
+	maxPrice = Math.ceil(maxPrice / 10);
+	priceInput.value = maxPrice;
+	priceInput.max = maxPrice;
+	priceInput.min = 0;
+	priceValue.innerHTML = `Value : <b>ETB</b> - ${maxPrice}`;
+	// console.log(maxPrice);
 
-    priceInput.addEventListener("input", function () {
-        const value = parseInt(priceInput.value);
-        console.log(value);
-        priceValue.innerHTML = `Value : <b>ETB</b> - ${value}`;
-        let newStore = store.filter((product) => product.price / 10 <= value);
-        display(newStore, getElement(".products-container"));
-        if (newStore.length < 1) {
-            const products = getElement(".products-container");
-            products.innerHTML = `<h3 class="filter-error"><b>
+	priceInput.addEventListener("input", function () {
+		const value = parseInt(priceInput.value);
+		console.log(value);
+		priceValue.innerHTML = `Value : <b>ETB</b> - ${value}`;
+		let newStore = store.filter((product) => product.price / 10 <= value);
+		display(newStore, getElement(".products-container"));
+		if (newStore.length < 1) {
+			const products = getElement(".products-container");
+			products.innerHTML = `<h3 class="filter-error"><b>
        sorry,</b> no products matched
        </h3>`;
-        }
-    });
+		}
+	});
 };
 
 export default setupPrice;
